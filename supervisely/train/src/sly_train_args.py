4b27fafc3bff514c5860bf3bb8113170402a2431
iwatkot <iwatkot@gmail.com>
2024-08-09 14:17:12 +0200

    Logging.

--- supervisely/train/src/sly_train_args.py
+++ supervisely/train/src/sly_train_args.py
@@ -6,15 +6,10 @@ import supervisely as sly
 def init_script_arguments(state):
     sly.logger.debug(f"Initing script arguments with state: {state}")
     sys.argv = [sys.argv[0]]
-
-    if g.temp_model_path is not None:
-        sys.argv.append(g.temp_model_path)
-        sly.logger.debug(f"Added model path argument with value {g.temp_model_path}")
-
     sys.argv.append(g.model_config_local_path)
+
     sys.argv.extend(["--workers", str(state["workersPerGPU"])])
     sys.argv.extend(["--batch-size", str(state["batchSizePerGPU"])])
     sys.argv.extend(["--weights", g.local_weights_path])
-
     if state["continueTrain"]:
         sys.argv.extend(["--resume-exp", "exp"])
