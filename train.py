e0f97f38411cd696e3158e15498e168e7fd76558
iwatkot <iwatkot@gmail.com>
2024-08-09 11:34:05 +0200

    Logging.

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import importlib.util
 
 import torch
 from isegm.utils.exp import init_experiment
+import supervisely as sly
 
 
 def main():
@@ -13,14 +14,14 @@ def main():
     else:
         model_script = load_module(args.model_path)
 
-    model_base_name = getattr(model_script, 'MODEL_NAME', None)
+    model_base_name = getattr(model_script, "MODEL_NAME", None)
 
     # TODO: dont forget
-    args.distributed = 'WORLD_SIZE' in os.environ
+    args.distributed = "WORLD_SIZE" in os.environ
     cfg = init_experiment(args, model_base_name)
 
     torch.backends.cudnn.benchmark = True
-    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.multiprocessing.set_sharing_strategy("file_system")
 
     model_script.main(cfg)
     return cfg
@@ -28,42 +29,73 @@ def main():
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    
-    parser.add_argument('model_path', type=str,
-                        help='Path to the model script.')
 
-    parser.add_argument('--exp-name', type=str, default='',
-                        help='Here you can specify the name of the experiment. '
-                             'It will be added as a suffix to the experiment folder.')
+    parser.add_argument("model_path", type=str, help="Path to the model script.")
 
-    parser.add_argument('--workers', type=int, default=4,
-                        metavar='N', help='Dataloader threads.')
+    parser.add_argument(
+        "--exp-name",
+        type=str,
+        default="",
+        help="Here you can specify the name of the experiment. "
+        "It will be added as a suffix to the experiment folder.",
+    )
 
-    parser.add_argument('--batch-size', type=int, default=-1,
-                        help='You can override model batch size by specify positive number.')
+    parser.add_argument("--workers", type=int, default=4, metavar="N", help="Dataloader threads.")
 
-    parser.add_argument('--ngpus', type=int, default=1,
-                        help='Number of GPUs. '
-                             'If you only specify "--gpus" argument, the ngpus value will be calculated automatically. '
-                             'You should use either this argument or "--gpus".')
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=-1,
+        help="You can override model batch size by specify positive number.",
+    )
 
-    parser.add_argument('--gpus', type=str, default='', required=False,
-                        help='Ids of used GPUs. You should use either this argument or "--ngpus".')
+    parser.add_argument(
+        "--ngpus",
+        type=int,
+        default=1,
+        help="Number of GPUs. "
+        'If you only specify "--gpus" argument, the ngpus value will be calculated automatically. '
+        'You should use either this argument or "--gpus".',
+    )
 
-    parser.add_argument('--resume-exp', type=str, default=None)
+    parser.add_argument(
+        "--gpus",
+        type=str,
+        default="",
+        required=False,
+        help='Ids of used GPUs. You should use either this argument or "--ngpus".',
+    )
 
-    parser.add_argument('--resume-prefix', type=str, default='latest',
-                        help='The prefix of the name of the checkpoint to be loaded.')
+    parser.add_argument("--resume-exp", type=str, default=None)
 
-    parser.add_argument('--start-epoch', type=int, default=0,
-                        help='The number of the starting epoch from which training will continue. '
-                             '(it is important for correct logging and learning rate)')
+    parser.add_argument(
+        "--resume-prefix",
+        type=str,
+        default="latest",
+        help="The prefix of the name of the checkpoint to be loaded.",
+    )
 
-    parser.add_argument('--weights', type=str, default=None,
-                        help='Model weights will be loaded from the specified path if you use this argument.')
+    parser.add_argument(
+        "--start-epoch",
+        type=int,
+        default=0,
+        help="The number of the starting epoch from which training will continue. "
+        "(it is important for correct logging and learning rate)",
+    )
 
-    parser.add_argument('--temp-model-path', type=str, default='',
-                        help='Do not use this argument (for internal purposes).')
+    parser.add_argument(
+        "--weights",
+        type=str,
+        default=None,
+        help="Model weights will be loaded from the specified path if you use this argument.",
+    )
+
+    parser.add_argument(
+        "--temp-model-path",
+        type=str,
+        default="",
+        help="Do not use this argument (for internal purposes).",
+    )
 
     parser.add_argument("--local_rank", type=int, default=0)
 
@@ -71,6 +103,9 @@ def parse_args():
 
 
 def load_module(script_path):
+    sly.logger.info(f"Loading model script from {script_path}...")
+    if not os.path.isfile(script_path):
+        raise FileNotFoundError(f"Model script not found: {script_path}")
     spec = importlib.util.spec_from_file_location("model_script", script_path)
     model_script = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(model_script)
@@ -78,5 +113,5 @@ def load_module(script_path):
     return model_script
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
